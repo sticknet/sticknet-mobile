619bca7ac5bc34cd5e0a20a2cdcff64ab9ecfb8f
Omar Basem <omarbasem.0101@gmail.com>
2025-01-01 18:50:12 +0400

    lint

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset', '@babel/preset-typescript'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: [
+        '@babel/preset-env', // Support modern JavaScript features
+        'module:metro-react-native-babel-preset', // Metro preset for React Native
+        '@babel/preset-typescript', // Support TypeScript syntax
+    ],
+    plugins: [
+        'react-native-reanimated/plugin', // Required for react-native-reanimated
+    ],
 };
